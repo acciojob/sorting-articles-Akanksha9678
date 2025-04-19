c1bceb89428842aad714b8d57f4eb1154619b318
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-20 02:43:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => {
+  const bandA = stripArticle(a);
+  const bandB = stripArticle(b);
+  return bandA.localeCompare(bandB);
+});
+
+let ul=document.querySelector("#band");
+
+for(let i=0;i<bands.length;i++){
+	let li=document.createElement("li");
+	li.innerText=bands[i];
+	ul.append(li);
+}
